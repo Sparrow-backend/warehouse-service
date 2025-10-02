bb671b378d196bb6bbbc317701b511115752f904
sliitguy <it23259416@my.sliit.lk>
2025-10-02 16:00:18 +0530

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const PORT = process.env.PORT || 8002
+const PORT = process.env.PORT || 8004
 
 mongoose.connection.once('open', () => {
     console.log("MongoDB is ready!")
