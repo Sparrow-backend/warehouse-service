100bab86a5fd60013b5fd318c66348ef2fca3d2b
sliitguy <it23259416@my.sliit.lk>
2025-10-02 15:59:20 +0530

    updated changes

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 const http = require('http')
+const app = require('./src/app')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const app = require('./src/app')
 
 dotenv.config()
 
-const PORT = process.env.PORT || 8004
-const MONGODB_URI = process.env.MONGODB_URI
-
-const server = http.createServer(app)
+const PORT = process.env.PORT || 8002
 
 mongoose.connection.once('open', () => {
-    console.log('MongoDB is ready!')
+    console.log("MongoDB is ready!")
 })
 
 mongoose.connection.on('error', () => {
-    console.log("Error in connecting with MongoDB!")
+    console.error('Error in connecting with MongoDB')
 })
+
+const server = http.createServer(app)
 
 async function createServer() {
     try {
-
-        mongoose.connect(MONGODB_URI)
-        console.log('Connnected to MongoDB!')
-
+        
         server.listen(PORT, () => {
-            console.log(`Warehouse Service: Listening on port ${PORT}..`)
+            console.log(`Server listening on port ${PORT}..`)
         })
 
+        
     } catch(err) {
-        console.error('Warehouse Service: Internal server error', err)
+        console.error('Internal server error: ', err)
+        process.exit(1)
     }
 }
 
 createServer()
+
+
--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors({
+    origin: [
+        'https://sparrow.nivakaran.dev',
+        'http://localhost:3000',
+        'http://nivakaran.dev'
+    ]
+}))
+
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({message: "Sparrow: Warehouse Service"})
+})
+
+app.get('/health', (req, res) => {
+    res.json({message: "Warehouse Service is running.."})
+})
+
 
 module.exports = app
