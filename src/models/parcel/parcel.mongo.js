99cfa82cd4b2e4c3dd88ea219b034582dd4e68bc
sliitguy <it23259416@my.sliit.lk>
2025-10-07 13:23:43 +0530

    updated parcel schema

--- src/models/parcel/parcel.mongo.js
+++ src/models/parcel/parcel.mongo.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+require('../warehouse/warehouse.mongo')
+require('../consolidation/consolidation.mongo')
+require('../user/user.mongo')
 
 const ParcelSchema = new mongoose.Schema({
     trackingNumber: {
@@ -42,6 +45,7 @@ const ParcelSchema = new mongoose.Schema({
             "created",
             "at_warehouse",
             "consolidated",
+            "assigned_to_driver",
             "in_transit",
             "out_for_delivery",
             "delivered",
@@ -73,11 +77,14 @@ const ParcelSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     },
+    assignedDriver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
     createdTimeStamp: {
         type: Date,
         default: Date.now
     }
-
 })
 
 module.exports = mongoose.model("Parcel", ParcelSchema);
